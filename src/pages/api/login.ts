0392c76f64f5e0876c7e516a187416167b9e7275
Davi Lucas <davilucasdlcreator@gmail.com>
2021-02-27 16:18:44 -0300

    :sparkles: feat github oauth working

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,24 +1,51 @@
-import { Octokit } from "@octokit/core";
 import axios from "axios";
+import dotenv from 'dotenv';
+import { NextApiResponse } from "next";
+import { serialize, CookieSerializeOptions } from "cookie";
+
+const setCookie = (
+  res: NextApiResponse,
+  name: string,
+  value: unknown,
+  options: CookieSerializeOptions = {}
+) => {
+  const stringValue =
+    typeof value === "object" ? "j:" + JSON.stringify(value) : String(value);
+
+  if ("maxAge" in options) {
+    options.expires = new Date(Date.now() + options.maxAge);
+    options.maxAge /= 1000;
+  }
+
+  options.path = '/';
+
+  res.setHeader("Set-Cookie", serialize(name, String(stringValue), options));
+};
 
 export default async function Login(req, res) {
-    let token;
-    const body = {
-        client_id: '4974563f2c8d1922e576',
-        client_secret: '3621e940ee14206f28726fbb0963db952892f1e6',
-        code: req.query.code,
-    };
-    const opts = { headers: { accept: "application/json" } };
-    axios
-        .post(`https://github.com/login/oauth/access_token`, body, opts)
-        .then((res) => res.data["access_token"])
-        .then((_token) => {
-            token = _token;
-            return;
-        })
-        .catch((err) => res.status(500).json({ message: err.message }));
+  dotenv.config();
 
-    const octokit = new Octokit({ auth: '5a964c806e2cc4f1921fe1642cf95ef25073b623' });
-    const responseOfApi = await octokit.request('GET https://api.github.com/user')
-    res.json(responseOfApi)
+  const { code } = req.query;
+  const { client_id,client_secret } = process.env;
+
+  const body = {
+    client_id,
+    client_secret,
+    code,
+  };
+
+  const opts = { headers: { accept: "application/json" } };
+  
+  const { data } = await axios.post(
+    `https://github.com/login/oauth/access_token`,
+    body,
+    opts
+  );
+
+  const userRequest = await axios.get("https://api.github.com/user", {
+    headers: { Authorization: `token ${data.access_token}` },
+  });
+
+  setCookie(res,"username", userRequest.data.login);
+  res.redirect('/cycle');
 }
