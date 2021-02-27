742a417fadcbe2eeab960f622c53e71b6b694e52
Davi Lucas <davilucasdlcreator@gmail.com>
2021-02-27 16:20:31 -0300

    :sparkles: feat framer-motion utils

---
+++ src/utils/motionProps.ts
@@ -0,0 +1,8 @@
+export const motionProps = {
+    variants: {
+      show: { opacity: 1, y: "0" },
+      hidden: { opacity: 0, y: "100%" },
+    },
+    initial: "hidden",
+    animate: "show",
+};
