f454a26f8cf8486837b4bce47dc48e375acf0012
Davi Lucas <davilucasdlcreator@gmail.com>
2021-03-01 19:23:37 -0300

    :sparkles: feat confetti on levelUpModal

--- src/components/LevelUpModal/index.tsx
+++ src/components/LevelUpModal/index.tsx
@@ -1,12 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import Confetti from 'react-canvas-confetti';
 import { ChallengesContext } from '../../contexts/ChallengesContext';
 import styles from '../../styles/components/LevelUpModal.module.css';
 import Link from '../Link';
 
 export default function LevelUpModal(){
     const { level,closeLevelUpModal } = useContext(ChallengesContext);
+    const [fire,setFire] = useState(false);
+    const [reset,setReset] = useState(false);
+
+    useEffect(() => {
+        setFire(true);
+        setTimeout(() => {
+            setReset(true);
+            setFire(false);
+        }, 4 * 1000)
+    },[])
+    
     return (
         <div className={styles.overlay}>
+            <Confetti
+                style={{ position:"fixed",width:"100%",height:"100%",zIndex:-1 }}
+                fire={fire}
+                reset={reset}
+            />
             <div className={styles.container}>
                 <header>{level}</header>
 
